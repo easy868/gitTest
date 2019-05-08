c9a3b0f470376bde294c2a65fb531dfbbd24f20d
jw <easy868@163.com>
2019-05-08 15:25:36 +0800

    新增了hell.vue

---
+++ src/components/hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>world</h1>
+    <h2 @click="$router.go(-1)">返回</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        now:'新建了hello.vue'
+      }
+    },
+    mounted () {
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello {
+    height: 1500px;
+    background: #666;
+  }
+</style>
